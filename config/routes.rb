b5ca0218f4e81f1321b73436d0283a485afb1c32
muhammadtashfeen92 <muhammad.hammad@piecyfer.com>
2024-03-29 16:26:29 +0500

    Completed Till 1 Hour And 34 Minutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :admin do
+    resources :orders
     resources :products do
       resources :stocks
     end
-      resources :categories
+    resources :categories
   end
   devise_for :admins
 
